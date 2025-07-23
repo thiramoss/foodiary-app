e0f3ee1f632af8f070b25d30793a6abd7f369d2c
thiramoss <trs.tiba@gmail.com>
2025-07-23 04:06:52 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import './styles/global.css';
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import { useEffect } from 'react';
+import './styles/global.css';
+import { HomeHeader } from './components/HomeHeader';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaProvider>
+        <Header />
+      </SafeAreaProvider>
+      <HomeHeader />
     </View>
   );
 }
