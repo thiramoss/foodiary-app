1f49991cafe8a7de40c87800cffc7823db0a957d
thiramoss <trs.tiba@gmail.com>
2025-09-07 13:30:06 -0300

    componente CameraModal

--- src/components/CreateMealBottomBar.tsx
+++ src/components/CreateMealBottomBar.tsx
@@ -4,12 +4,15 @@ import { Button } from "./Button";
 import { CameraIcon, MicIcon } from "lucide-react-native";
 import { useState } from "react";
 import { AudioModal } from "./AudioModal";
+import { CameraModal } from "./CameraModal";
 
 export function CreateMealBottomBar() {
 
     const { bottom } = useSafeAreaInsets();
 
     const [isAudioModalOpen, setIsAudioModalOpen ] = useState(false);
+    const [isPictureModalOpen, setIsPictureModalOpen ] = useState(false);
+
 
     return (
         <View className="absolute z-10 w-full bottom-0 bg-white h-20 border-gray-400 pt-4"
@@ -20,12 +23,14 @@ export function CreateMealBottomBar() {
                     <MicIcon />
                 </Button>
 
-                <Button size="icon" color="gray">
+                <Button size="icon" color="gray" onPress={() => setIsPictureModalOpen(true)}>
                     <CameraIcon />
                 </Button>
             </View>
 
             <AudioModal open={isAudioModalOpen} onClose={() => setIsAudioModalOpen(false)}/>
+
+            <CameraModal open={isPictureModalOpen} onClose={() => setIsPictureModalOpen(false)}/>
 
         </View>
     )
