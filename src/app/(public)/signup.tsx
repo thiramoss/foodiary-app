779fd6cfe5ac952bfcea7a6507419ebb3371a507
thiramoss <trs.tiba@gmail.com>
2025-10-09 04:09:37 -0300

    React Hook Form no SignUp

--- src/app/(public)/signup.tsx
+++ src/app/(public)/signup.tsx
@@ -1,9 +1,83 @@
 import { Text, View } from "react-native";
+import { AuthLayout } from "../../components/AuthLayout";
+import { useState } from "react";
+import { GoalStep } from "../../components/SignUpSteps/GoalStep";
+import { GenderStep } from "../../components/SignUpSteps/GenderStep";
+import { Button } from "../../components/Button";
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react-native";
+import { colors } from "../../styles/colors";
+import { router } from "expo-router";
+import z from "zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema } from "../../components/SignUpSteps/signUpSchema";
 
-export default function SingIn(){
-  return(
-    <View>
-      <Text>Entrar</Text>
-    </View>
+export default function SingUp() {
+
+
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+  } )
+
+  const steps = [
+    {
+      icon: '🎯',
+      title: 'Qual é o seu objetivo',
+      subtitle: 'O que você pretende alcançar com a dieta?',
+      Component: GoalStep,
+    },
+    {
+      icon: '👥',
+      title: 'Qual é o seu gênero?',
+      subtitle: 'Seu gênero influencia no tipo da dieta?',
+      Component: GenderStep,
+    }
+  ];
+
+  function handlePreviousStep() {
+    if (currentStepIndex === 0) {
+      router.back();
+      return;
+    }
+    setCurrentStepIndex(prevState => prevState - 1);
+  }
+
+  function handleNextStep() {
+    setCurrentStepIndex(prevState => prevState + 1);
+
+  }
+
+
+  const currentStep = steps[currentStepIndex];
+
+  return (
+    <AuthLayout
+      icon={currentStep.icon}
+      title={currentStep.title}
+      subtitle={currentStep.subtitle}>
+
+      <View className="flex-1 justify-between">
+        <FormProvider {...form}>
+          <currentStep.Component />
+        </FormProvider>
+        
+
+        <View className="flex-row justify-between">
+          <Button size="icon" color="gray" onPress={handlePreviousStep}>
+            <ArrowLeftIcon size={20} color={colors.black[700]} />
+          </Button>
+
+          <Button size="icon" onPress={handleNextStep}>
+            <ArrowRightIcon size={20} color={colors.black[700]} />
+          </Button>
+
+        </View>
+
+      </View>
+
+
+    </AuthLayout>
   )
 }
